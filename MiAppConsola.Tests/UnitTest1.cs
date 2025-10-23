681cd217cdaa59dcd9829a4cd611d465bdd5fbe1
unknown <marioalejop@gmial.com>
2025-10-23 13:26:41 -0500

    Testing Dotnet

---
+++ MiAppConsola.Tests/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using Xunit;
+using System;
+
+namespace MiAppConsola.Tests
+{
+
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Sumar_DosNumeros_RetornaResultadoEsperado()
+        {
+            var calculadora = new Calculadora();
+            int a = 5;
+            int b = 3;
+
+            int resultado = calculadora.Sumar(a, b);
+
+            Assert.Equal(8, resultado);
+
+        }
+
+        [Fact]
+        public void Restar_DosNumeros_RetornaResultadoEsperado()
+        {
+            var calculadora = new Calculadora();
+            int a = 5;
+            int b = 3;
+
+            int resultado = calculadora.Restar(a, b);
+
+            Assert.Equal(2, resultado);
+        }
+
+        [Fact]
+        public void Multiplicar_DosNumeros_RetornaResultadoEsperado()
+        {
+            var calculadora = new Calculadora();
+            int a = 5;
+            int b = 3;
+
+            int resultado = calculadora.Multiplicar(a, b);
+
+            Assert.Equal(15, resultado);
+        }
+        
+        [Fact]
+        public void Dividir_DosNumeros_RetornaResultadoEsperado()
+        {
+            var calculadora = new Calculadora();
+            int a = 6;
+            int b = 3;
+
+            int resultado = calculadora.Dividir(a, b);
+
+            Assert.Equal(2, resultado);
+        }
+
+    }
+}
---
+++ MiAppConsola/Program.cs
@@ -0,0 +1,39 @@
+﻿using System; 
+namespace MiAppConsola
+{
+    public class Calculadora
+    {
+        public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Restar(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+        
+        public int Dividir(int a, int b)
+        { 
+            return a / b;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculadora();
+            Console.WriteLine("5 + 3 = " + calc.Sumar(5, 3));
+            Console.WriteLine("5 - 3 = " + calc.Restar(5, 3));
+            Console.WriteLine("5 * 3 = " + calc.Multiplicar(5, 3));
+            Console.WriteLine("6 / 3 = " + calc.Dividir(6, 3));
+            
+        }
+    }
+}
