0f2af2ea7560f5981376ca0b33e100785426b6ba
unknown <marioalejop@gmail.com>
2025-10-23 21:28:44 -0500

    Agregar resultafo

--- PrubasUnitarias/testUnit/Tests.cs
+++ PrubasUnitarias/testUnit/Tests.cs
@@ -1,14 +1,17 @@
-﻿using testUnit.Data;
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using testUnit.Data;
 
 namespace testUnit;
 
 public class Tests
 {
     [Test]
-    public void testSuma()
+    public async Task testSuma()
     {
         var Calculadora = new Calculadora();
-        var resultado = Calculadora.Sumar(1,3);
+        var resultado = Calculadora.Sumar(1, 3);
+        await Assert.That(resultado).IsEqualTo(4);
         Console.WriteLine();
     }
 
--- PrubasUnitarias/testUnit/obj/Debug/net8.0/testUnit.AssemblyInfo.cs
+++ PrubasUnitarias/testUnit/obj/Debug/net8.0/testUnit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testUnit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e19489ca317f833a6bfd5674c9d9fe03f705d14")]
 [assembly: System.Reflection.AssemblyProductAttribute("testUnit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testUnit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
